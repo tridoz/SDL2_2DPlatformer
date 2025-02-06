a91858d2eb502d5da121dccb5e1982675668efa4
tridoz <149873185+tridoz@users.noreply.github.com>
2025-02-06 09:51:13 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ostream>
+
+#include "src/Game_Engine.h"
+
+int main(int argc, char** argv) {
+    Game_Engine game_engine("Game", 1536, 1024);
+
+    game_engine.Init();
+
+    if ( game_engine.errorHasOccurred() ) {
+        std::cout<<"Cause< "<<game_engine.getErrorCause()<<" >"<<std::endl;
+        std::cout<<"Error< "<<game_engine.getErrorMessage()<<" >"<<std::endl;
+        return 10;
+    }
+
+    game_engine.run();
+
+    if ( game_engine.errorHasOccurred() ) {
+        std::cout<<"Cause< "<<game_engine.getErrorCause()<<" >"<<std::endl;
+        std::cout<<"Error< "<<game_engine.getErrorMessage()<<" >"<<std::endl;
+        return -1;
+    }
+
+    return 0;
+}
